33c8279ac5d882ffc1ea35568c560aaad8375184
Jeroen van Maanen <jeroen@sollunae.net>
2017-11-13 11:25:13 +0100

    Improved build script for epistemics.

--- epistemics/bin/docker-build.sh
+++ epistemics/bin/docker-build.sh
@@ -1,29 +1,42 @@
 #!/bin/bash
 
-## /artifactory/libs-release-local/selemca/epistemics-beliefsysteem-rest/1.2/epistemics-beliefsysteem-rest-1.2.war
-
 set -e -x
 
 BIN="$(cd "$(dirname "$0")" ; pwd)"
 PROJECT="$(dirname "${BIN}")"
 
-VERSION='1.2'
-ARTIFACTORY_IP="$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' artifactory)"
-ARTIFACTORY_URL="http://${ARTIFACTORY_IP}:8081/artifactory/libs-release-local/selemca"
+VERSION='1.3.0-SNAPSHOT'
+ARTIFACTORY_IP="$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' artifactory 2>/dev/null || true)"
+ARTIFACTORY_URL=''
+if [ -n "${ARTIFACTORY_IP}" ]
+then
+	ARTIFACTORY_URL="http://${ARTIFACTORY_IP}:8081/artifactory/libs-release-local/selemca"
+fi
+LOCAL_REPO="${HOME}/.m2/repository"
 
 function download-artifact() {
 	local ARTIFACT_ID="$1"
 	local PACKAGE_ID="$2"
-	curl -L -sS -o "${PACKAGE_ID}" "${ARTIFACTORY_URL}/${ARTIFACT_ID}/${PACKAGE_ID}"
+	local VERSION="$3"
+	if [ -n "${ARTIFACTORY_URL}" ]
+	then
+		curl -L -sS -o "${PACKAGE_ID}" "${ARTIFACTORY_URL}/${ARTIFACT_ID}/${PACKAGE_ID}"
+	elif [ -d "${LOCAL_REPO}" ]
+	then
+		cp "${LOCAL_REPO}/selemca/${ARTIFACT_ID}/${PACKAGE_ID}" "${PACKAGE_ID}"
+	else
+		return 1
+	fi
 }
 
 (
 	cd "${PROJECT}/docker"
 
-	download-artifact "epistemics-beliefsysteem-rest/${VERSION}" "epistemics-beliefsysteem-rest-${VERSION}.war"
-	download-artifact "epistemics-beliefsystem-webadmin/${VERSION}" "epistemics-beliefsystem-webadmin-${VERSION}.war"
-	download-artifact "epistemics-mentalworld-webapp/${VERSION}" "epistemics-mentalworld-webapp-${VERSION}.war"
-	download-artifact "epistemics-mentalworld-webadmin/${VERSION}" "epistemics-mentalworld-webadmin-${VERSION}.war"
+	download-artifact "epistemics-beliefsystem-rest/${VERSION}" "epistemics-beliefsystem-rest-${VERSION}.war" "${VERSION}"
+	download-artifact "epistemics-beliefsystem-webadmin/${VERSION}" "epistemics-beliefsystem-webadmin-${VERSION}.war" "${VERSION}"
+	download-artifact "epistemics-mentalworld-webapp/${VERSION}" "epistemics-mentalworld-webapp-${VERSION}.war" "${VERSION}"
+	download-artifact "epistemics-mentalworld-webadmin/${VERSION}" "epistemics-mentalworld-webadmin-${VERSION}.war" "${VERSION}"
+	download-artifact "epistemics-mentalworld-rest/${VERSION}" "epistemics-mentalworld-rest-${VERSION}.war" "${VERSION}"
 
 	docker build -t jeroenvm/epistemics .
 )
