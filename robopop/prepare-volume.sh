cd2b39289bf6c9d8e30a0706ab5c7603be94e042
Jeroen van Maanen <jeroen@sollunae.net>
2017-11-13 12:32:48 +0100

    Fixed prefix of volume name to match new directory name.

--- robopop/prepare-volume.sh
+++ robopop/prepare-volume.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+set -e
+
 BIN="$(cd "$(dirname "$0")" ; pwd)"
 DOCKER="$(dirname "${BIN}")"
 EPISTEMICS="${DOCKER}/epistemics"
 
+. "${DOCKER}/bin/verbose.sh"
+
 (
     cd "${EPISTEMICS}/data/selemca"
-    docker run -v stack_epistemics-config:/usr/local/selemca --name helper busybox true
+    docker run -v robopop_epistemics-config:/usr/local/selemca --name helper busybox true
     docker cp . helper:/usr/local/selemca
     docker rm helper
 )
