d8507083217007d147e13b6dc444297193e6fe74
Jeroen van Maanen <jeroen@sollunae.net>
2018-05-14 14:46:51 +0200

    Added variables "${SICO_SUFFIX}" and "${DISPLAY}" to "docker-compose-template.yml" and "docker-compose-up.sh". Fixed function "re-protect" in "docker-compose-up.sh".

--- robopop/docker-compose-up.sh
+++ robopop/docker-compose-up.sh
@@ -24,15 +24,28 @@ source "${SILICON_COPPELIA}/etc/settings-local.sh"
 BASE="${COMPOSE}/docker-compose"
 TEMPLATE="${BASE}-template.yml"
 TARGET="${BASE}.yml"
+SICO_SUFFIX=''
+: ${DISPLAY:=172.16.123.1:0}
+if [ ".$1" = '.--gui' ]
+then
+    SICO_SUFFIX='-gui'
+    shift
+    if [ ".$1" = '.-d' ]
+    then
+        export DISPLAY="$2"
+        shift 2
+    fi
+fi
 VARIABLES="$(tr '$\012' '\012$' < "${TEMPLATE}" | sed -e '/^[{][A-Za-z_][A-Za-z0-9_]*[}]/!d' -e 's/^[{]//' -e 's/[}].*//')"
 
 function re-protect() {
-    sed "${SED_EXT}" -e 's/([[]|[]]|[|*?^$()])/\\\1/'
+    sed "${SED_EXT}" -e 's/([[]|[]]|[|*?^$()/])/\\\1/g'
 }
 
 function substitute() {
     local VARIABLE="$1"
     local VALUE="$(eval "echo \"\${${VARIABLE}}\"" | re-protect)"
+    log "VALUE=[${VALUE}]"
     if [ -n "$(eval "echo \"\${${VARIABLE}+true}\"")" ]
     then
         sed "${SED_EXT}" -e "s/[\$][{]${VARIABLE}[}]/${VALUE}/g" "${TARGET}" > "${TARGET}~"
