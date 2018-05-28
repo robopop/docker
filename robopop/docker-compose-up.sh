2ffcbdf7c5bcde0a54b27a557cc9bb0502d76be7
Jeroen van Maanen <jeroen@sollunae.net>
2018-05-28 12:14:38 +0200

    Added "docker-compose-gui-template.yml" that specifies a bind volume for the Silicon Coppélia examples. Added Silicon Coppélia example files.

--- robopop/docker-compose-up.sh
+++ robopop/docker-compose-up.sh
@@ -22,9 +22,6 @@ source "${EPISTEMICS}/etc/settings-local.sh"
 source "${EPISTEMICS}/etc/credentials-local.sh"
 source "${SILICON_COPPELIA}/etc/settings-local.sh"
 
-BASE="${COMPOSE}/docker-compose"
-TEMPLATE="${BASE}-template.yml"
-TARGET="${BASE}.yml"
 SICO_SUFFIX=''
 : ${DISPLAY:=172.16.123.1:0}
 if [ ".$1" = '.--gui' ]
@@ -37,6 +34,16 @@ then
         shift 2
     fi
 fi
+if [ ".$1" = '.-e' ]
+then
+    SICO_EXAMPLES="$2"
+    shift 2
+fi
+: ${SICO_EXAMPLES:=../sico/examples}
+SICO_DIST="$(docker run --rm jeroenvm/silicon-coppelia-gui:latest /bin/pwd)"
+BASE="${COMPOSE}/docker-compose"
+TEMPLATE="${BASE}${SICO_SUFFIX}-template.yml"
+TARGET="${BASE}.yml"
 VARIABLES="$(tr '$\012' '\012$' < "${TEMPLATE}" | sed -e '/^[{][A-Za-z_][A-Za-z0-9_]*[}]/!d' -e 's/^[{]//' -e 's/[}].*//')"
 
 function re-protect() {
